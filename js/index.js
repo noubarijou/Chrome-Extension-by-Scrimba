133bea0f11c6e3ee280ee8d700e7f47d8eedbbcb
noubarijou <85699669+noubarijou@users.noreply.github.com>
2021-08-04 15:14:19 -0300

    adds js functions and styling

--- js/index.js
+++ js/index.js
@@ -1,15 +1,24 @@
+let myLeads = [];
+const inputEl = document.getElementById("input-el");
+let ulEl = document.getElementById("ul-el");
 
-let myLeads = ['awesomelead', 'epiclead', 'greatlead'];
-const inputEl = document.getElementById('input-el');
-let ulEl = document.getElementById('ul-el');
-
-const inputBtn = document.getElementById('input-btn');
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value);
-    
-})
-
-
-for (let i = 0; i < myLeads.length; i++) {
-    ulEl.innerHTML += "<li>" + myLeads[i] + "</li>";
+const inputBtn = document.getElementById("input-btn");
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  renderLeads();
+});
+function renderLeads() {
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    listItems += `
+    <li><a target='_blank' href='${myLeads[i]}'>
+    ${myLeads[i]}
+    </a>
+    </li>`;
+    /* const li = document.createElement("li");
+    li.textContent = myLeads[i];
+    ulEl.append(li); */
+  }
+  ulEl.innerHTML = listItems;
 }
