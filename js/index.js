1fbc2f4823176a60d65f2a8a485711567bb9eaac
noubarijou <85699669+noubarijou@users.noreply.github.com>
2021-08-04 16:18:06 -0300

    adds logo, delete function, refactors js code

--- js/index.js
+++ js/index.js
@@ -1,24 +1,39 @@
 let myLeads = [];
 const inputEl = document.getElementById("input-el");
 let ulEl = document.getElementById("ul-el");
-
 const inputBtn = document.getElementById("input-btn");
-inputBtn.addEventListener("click", function () {
-  myLeads.push(inputEl.value);
-  inputEl.value = "";
-  renderLeads();
-});
-function renderLeads() {
+const deleteBtn = document.getElementById("delete-btn");
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+
+if (leadsFromLocalStorage) {
+  myLeads = leadsFromLocalStorage;
+  render(myLeads);
+}
+
+function render(leads) {
   let listItems = "";
-  for (let i = 0; i < myLeads.length; i++) {
+  for (let i = 0; i < leads.length; i++) {
     listItems += `
-    <li><a target='_blank' href='${myLeads[i]}'>
-    ${myLeads[i]}
-    </a>
-    </li>`;
+      <li><a target='_blank' href='${leads[i]}'>
+      ${leads[i]}
+      </a>
+      </li>`;
     /* const li = document.createElement("li");
-    li.textContent = myLeads[i];
-    ulEl.append(li); */
+      li.textContent = myLeads[i];
+      ulEl.append(li); */
   }
   ulEl.innerHTML = listItems;
 }
+
+deleteBtn.addEventListener("dblclick", function () {
+  localStorage.clear();
+  myLeads = [];
+  render(myLeads);
+});
+
+inputBtn.addEventListener("click", function () {
+  myLeads.push(inputEl.value);
+  inputEl.value = "";
+  localStorage.setItem("myLeads", JSON.stringify(myLeads));
+  render(myLeads);
+});
